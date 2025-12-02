d78675a677031c4d9809b39e966639685666986d
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-12-02 05:43:38 +0100

    Dev: Cleaning / page content

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="page-title">
+        Welcome.
+      </h1>
+      <p className="page-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla delectus, illum maiores unde nihil iste facilis accusamus quibusdam voluptates reiciendis aliquam libero amet blanditiis porro ullam explicabo error? Deleniti impedit laudantium dolores autem eius incidunt modi eveniet aliquam possimus assumenda voluptatum veniam, sapiente nulla magnam quidem atque pariatur quo excepturi cupiditate.
+      </p>
+    </div>
+  );
+}
